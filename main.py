e53bed73468e20c86175e9fca666cd96fdfd644b
DawidGlazik <dawid-glazik@wp.pl>
2024-09-04 11:31:20 +0200

    Kernel_svm

--- main.py
+++ main.py
@@ -44,9 +44,15 @@ Test = np.array(ct.fit_transform(Test))
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+Test = sc.transform(Test)
+
 # training the model
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy', random_state = 0)
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # predicting test set results (train.csv)
